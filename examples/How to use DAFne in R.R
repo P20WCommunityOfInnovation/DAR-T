b2a5643b63423aa8cce2844798e5ec9cc93ddcdc
Amstutz <AmstutzG1@michigan.gov>
2024-01-25 08:25:38 -0500

    Added R script which shows an example of calling DAFne into R

---
+++ examples/How to use DAFne in R.R
@@ -0,0 +1,25 @@
+#### How to run DAFne from your R file ###
+
+# import reticulate package, which enables you to access and run Python
+library(reticulate)
+
+# load in the DataAnonymizer class with source_python
+source_python("..\\packages\\suppression_check.py")
+
+# read in your data you would like to redact
+df <- read.csv("..\\data\\UserData2.csv")
+
+# initialize parameters to be used in DataAnonymizer call
+sensitive_list <- list('Subgroup1', 'Subgroup2')
+frequency_value <- 'Counts'
+
+# initialize the anonymizer
+anonymizer <- DataAnonymizer(df, sensitive_columns = sensitive_list, 
+                            frequency = frequency_value, redact_column = 'UserRedact')
+
+# call the function apply_anonymization and save to a new dataframe
+df_redacted <- anonymizer$apply_anonymization()
+
+# view redacted dataframe
+View(df_redacted)
+
