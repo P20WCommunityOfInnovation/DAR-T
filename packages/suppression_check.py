bac787bc4df9a4d24728591cf19a21d716386738
VT-AOE-DMAD-Drew-Bennett <drew.bennett@vermont.gov>
2024-02-26 14:32:57 -0500

    Removed print statements

--- packages/suppression_check.py
+++ packages/suppression_check.py
@@ -468,9 +468,7 @@ class DataAnonymizer:
             columns = self.sensitive_columns +  [self.frequency] + ['RedactBinary', 'Redact', 'RedactBreakdown']
 
         if self.redact_column is not None:
-            print(columns)
             columns = columns + self.redact_column
-            print(columns)
         
         df_redacted = df_redacted[columns]
         # df_redacted = df_redacted.drop_duplicates().reset_index(drop=True) # this helps remove the duplicate issue, but the duplicates should not be there
