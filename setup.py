75047de07b7243a58888f713ea0494e9a34cc2d6
VT-AOE-DMAD-Drew-Bennett <drew.bennett@vermont.gov>
2024-03-01 09:33:01 -0500

    dar_t to dar-t

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='dar_t',
+    name='dar-t',
     version='0.1.0',
     author='P20W+ Community of Innovation',
     author_email='info@communityofinnovation.org',
