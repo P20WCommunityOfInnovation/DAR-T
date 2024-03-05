efacac6db4a940ea45466de6ef58a1dd013cc564
sethltaylor <setht1191@gmail.com>
2024-03-05 16:04:28 -0500

    Updates to layout + CD pipeline

--- app/streamlit_app.py
+++ app/streamlit_app.py
@@ -12,13 +12,20 @@ st.set_page_config(
     }
 )
 
-# Add columsn for center
-st.image(image="images/DAR-T_main_text.png", width=500)
+# Add columns for centering image
+left, middle, right = st.columns(3)
+
+with middle:
+    st.image(image="images/DAR-T_main_text.png", width = 1000)
 
 st.header("This tool is designed to support users with redacting sensitive records in aggregate files. By default this tool will redact records where the count is 10 or less and all additional records needed for complimentary suppression.")
 
 st.subheader("Upload your .csv or .xlsx file with aggregates to get started:")
-uploadedFile = st.file_uploader("Upload file", type=['csv','xlsx'],accept_multiple_files=False,key="fileUploader")
+
+#Creating columns so file upload widget does not span entire page. 
+filecol,unusedcol = st.columns([.3,.7])
+with filecol:
+    uploadedFile = st.file_uploader("Upload file", type=['csv','xlsx'],accept_multiple_files=False,key="fileUploader")
 
 if uploadedFile:
     if uploadedFile.name.endswith('.csv'):
@@ -29,11 +36,8 @@ if uploadedFile:
     else:
         raise Exception("Your uploaded file must be a .csv or .xlsx")
     
-    st.header("Un-redacted file")
-    st.subheader("Verify accuracy of the file before continuing.")
-    st.write(df)
-
     
+    #Create sidebar for user to specify inupts to redaction function
     with st.sidebar:
         st.sidebar.title("Inputs for Redaction")
         st.header("Select your function inputs")
@@ -59,7 +63,58 @@ if uploadedFile:
             st.caption("If you do not specify a value, columns will be marked for redaction but original counts will still be shown.")
             st.caption("Leaving the box checked but no value specified will overwrite values with a blank.")
 
-#Format user provided values as needed
+ 
+    #Creating layout for 
+    st.header("Un-redacted file")
+    st.subheader("Verify accuracy of the file before continuing.")
+
+    st.write(df)
+
+    st.subheader("Once you have verified your input file, select the appropriate columns in the file as inputs to the function in the sidebar. An explanation of each option is included below.")
+
+    #Create columns for organizing explainer text
+    col1, col2, col3 = st.columns([1.5,1,1.5])
+
+    with col1:
+        st.markdown(
+            """
+            #### Grouping Columns
+
+            **Parent Organization**: The top level grouping for your aggregates. This may be a category like "School District". This is optional. 
+
+            **Child Organization**: A subgroup for your aggregates. This may be a category like "School". This is optional. 
+
+            **Sensitive Columns**: Columns that group individual records into an aggregate. This would be a category like "Gender" or "IEP Status". At least one column must be specified. 
+
+            *While parent organization and child organization are optional, you may not have duplicate values across your grouping columns. E.g., You cannot have more than one record for "English Language Learner - Male".*  
+              
+            *If you do have duplicates, you should specify a child organization or parent organization column to disaggregate these values. E.g., School 1 - English Language Learner - Male, School 2 - English Language Learner - Male*
+            """)
+    with col2:
+        st.markdown(
+            """
+            #### Count Column
+
+            **Aggregate Count Colum**: The column that contains your aggregate counts to be suppressed. 
+            """)
+    with col3:
+        st.markdown(
+            """
+            #### Additional User Inputs
+
+            **User Specified Redaction Column**: An optional column where the user may pre-specify which columns they want redaction applied to. Columns marked for redaction must contain be marked with a 1. 
+
+            **Minimum Threshold**: The minimum threshold for redaction. This is 10 by default and does not include zero. All values 10 and below, except zero, will be redacted. 
+
+            **Should zeroes be redacted?**: If checked, zeroes will be redacted in addition to values at or below the minimum threshold. 
+
+            **What string should replace redacted values?**: An optional string value you can specify that will overwrite redacted counts with the specified value.
+            """
+        )
+
+    st.subheader("Click 'Redact my dataset' in the sidebar when you are ready to redact your file.")
+
+    #Format user provided values as needed
     if parent_org == 'None':
         parent_org = None
 
@@ -76,7 +131,7 @@ if uploadedFile:
 
 #Add button to apply redaction
     
-    if st.button("Redact my dataset"):
+    if st.sidebar.button("Redact my dataset"):
         anonymizer = DataAnonymizer(df, parent_organization=parent_org, child_organization=child_org,sensitive_columns=sensitive_columns, frequency= frequency, redact_column=redact_column,redact_zero=redact_zero,redact_value= redact_value)
         df_redacted = anonymizer.apply_anonymization()
 
@@ -85,6 +140,7 @@ if uploadedFile:
         st.subheader("The file can be downloaded via the download icon in the top right of the table.")
         st.write(df_redacted)
 
+    
 
     
     
